5730773e38957a038e1e64e8c5fb6ac0aa38a052
Ashish Singh <ashishcodes4@gmail.com>
2018-04-13 07:46:09 +0530

    editing cam file

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,44 @@
+console.log('connected');
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+const getVideo = () => {
+  navigator.mediaDevices.getUserMedia({video:true, audio:true})
+  .then(localMediaStream => {
+    console.log(localMediaStream);
+    video.src = window.URL.createObjectURL(localMediaStream);
+    video.play();
+  })
+  .catch(err => {
+    alert('OH NO, Please grant access to webcam.');
+    console.log(err);
+  });
+}
+
+const paintToCanvas = () => {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0,0, width, height);
+  }, 16);
+}
+
+const takePhoto = () => {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('downlaod', 'MAfiA');
+  link.textContent = 'Download Image';
+  strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();

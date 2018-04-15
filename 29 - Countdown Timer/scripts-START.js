4e518d8b171db2c8632664d6bba333033b0698d0
Ashish Singh <ashishcodes4@gmail.com>
2018-04-15 10:38:40 +0530

    function to measure time created

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -4,6 +4,7 @@ const timerDisplay  = document.querySelector('.display__time-left');
 const timer = (seconds) => {
   const now = Date.now();
   const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
 
   countDown = setInterval(() => {
     //decide what the countDown will start with...
@@ -13,14 +14,14 @@ const timer = (seconds) => {
       clearInterval(countDown);
       return;
     }
-    console.log(secondsLeft);
+    displayTimeLeft(secondsLeft);
   }, 1000);
 }
 
-function displayTimeLeft(seconds) {
+const displayTimeLeft = (seconds) => {
   const minutes = Math.floor(seconds/60);
   const remainderSeconds = seconds % 60;
-  const display  = `${minutes}: ${remainderSeconds < 10 ? '10' : ''} ${remainderSeconds}`;
+  const display  = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
   document.title = display;
-
+  timerDisplay.textContent = display;
 }
